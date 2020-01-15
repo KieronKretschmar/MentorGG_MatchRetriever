56b8920fe6673e841b79b07dc09e877e151f20e1
Kieron <kieron@mentor.gg>
2020-01-15 10:41:03 +0100

    Init solution with TestControllers and versioning

---
+++ MatchRetriever/Controllers/api/v1/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchRetriever.Controllers.api.v1
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : BaseApiController
+    {
+        [HttpGet]
+        public string Get() => "Version 1";
+    }
+}
---
+++ MatchRetriever/Controllers/api/v2/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchRetriever.Controllers.api.v2
+{
+    [ApiVersion("2")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Version 2";
+    }
+}
