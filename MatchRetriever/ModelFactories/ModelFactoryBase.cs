6720be3f3dba8fb25d06ccabaab0a169828f5932
Kieron <kieron@mentor.gg>
2020-01-20 10:19:44 +0100

    Add ModelFactoryBase.cs to serve as base class for services that access data and build models, in order to lay foundation for repository pattern.

---
+++ MatchRetriever/ModelFactories/ModelFactoryBase.cs
@@ -0,0 +1,27 @@
+﻿using Database;
+using MatchRetriever.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.ModelFactories
+{
+    public class ModelFactoryBase
+    {
+        protected readonly ILogger<ModelFactoryBase> _logger;
+        protected readonly ISteamUserOperator _steamUserOperator;
+        protected readonly MatchContext _context;
+        protected readonly IServiceProvider _sp;
+
+        public ModelFactoryBase(IServiceProvider sp)
+        {
+            _logger = sp.GetRequiredService<ILogger<ModelFactoryBase>>();
+            _steamUserOperator = sp.GetRequiredService<ISteamUserOperator>();
+            _context = sp.GetRequiredService<MatchContext>();
+            _sp = sp;
+        }
+    }
+}
