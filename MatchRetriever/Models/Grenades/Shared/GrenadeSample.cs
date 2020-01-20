8943c475fa4b6bc07a13125c6855ecc3c52f30ec
Kieron <kieron@mentor.gg>
2020-01-20 10:33:51 +0100

    Add grenade and zone related base classes

---
+++ MatchRetriever/Models/Grenades/Shared/GrenadeSample.cs
@@ -0,0 +1,19 @@
+﻿using MatchRetriever.Helpers.Trajectories;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace MatchRetriever.Models.Grenades
+{
+    public class GrenadeSample
+    {
+        public long MatchId { get; set; }
+        public long PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public long GrenadeId { get; set; }
+        public short Round { get; set; }
+        public bool UserIsCt { get; set; }
+        public Vector3 Release { get; set; }
+        public Vector3 Detonation { get; set; }
+        public List<TrajectoryPoint> Trajectory { get; set; }
+    }
+}
---
+++ MatchRetriever/Models/Grenades/Shared/SampleZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatchRetriever.Models.Grenades
+{
+    public class SampleZone
+    {
+        public int ZoneId { get; set; }
+        public string Name { get; set; }
+        public bool IsCtZone { get; set; }
+        public float CenterXPixel { get; set; }
+        public float CenterYPixel { get; set; }
+        public List<int> PolygonPointsX { get; set; } = new List<int>();
+        public List<int> PolygonPointsY { get; set; } = new List<int>();
+        public int ParentZoneId { get; set; }
+        public int Depth { get; set; }
+    }
+}
