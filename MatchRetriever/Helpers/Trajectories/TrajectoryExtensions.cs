036c3049c8da5f393ba4cee80a378e43215e1d0c
Kieron <kieron@mentor.gg>
2020-01-20 10:16:08 +0100

    Add TrajectoryPoint.cs to (de-)serialize trajectories from database. The file could be moved to MatchEntities

---
+++ MatchRetriever/Helpers/Trajectories/TrajectoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.Helpers.Trajectories
+{
+    public static class TrajectoryExtensions
+    {
+        public static bool IsAlmostEqual(this List<TrajectoryPoint> points, List<TrajectoryPoint> otherPoints)
+        {
+            if (otherPoints == null || points.Count != otherPoints.Count)
+                return false;
+
+            return points
+                .Zip(otherPoints, (thisPoint, otherPoint) => thisPoint.IsAlmostEqual(otherPoint))
+                .All(x => x);
+        }
+    }
+}
