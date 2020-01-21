34223f40d04b23e52aba3c59808f912f037fb314
Kieron <kieron@mentor.gg>
2020-01-21 11:43:41 +0100

    Improve test project structure

--- MatchRetrieverTestProject/BasicTests.cs
+++ MatchRetrieverTestProject/BasicTests.cs
@@ -12,19 +12,19 @@ namespace MatchRetrieverTestProject
 
         public BasicTests()
         {
-            //_factory = factory;
             _factory = new WebApplicationFactory<Startup>();
         }
 
         [DataTestMethod]
-        [DataRow("/v1/public/single/flashes?steamId=76561198033880857&map=de_mirage&matchIds=1,2,3")]
-        [DataRow("/v1/public/single/flashesoverview?steamId=76561198033880857&matchIds=1,2,3")]
-        [DataRow("/v1/public/single/firenades?steamId=76561198033880857&map=de_mirage&matchIds=1,2,3")]
-        [DataRow("/v1/public/single/firenadesoverview?steamId=76561198033880857&matchIds=1,2,3")]
-        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        [DataRow("flashes")]
+        [DataRow("flashesoverview")]
+        [DataRow("firenades")]
+        [DataRow("firenadesoverview")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string grenadeString)
         {
             // Arrange
             var client = _factory.CreateClient();
+            var url = GetParametrizedGrenadeUrl(grenadeString);
 
             // Act
             var response = await client.GetAsync(url);
@@ -33,6 +33,22 @@ namespace MatchRetrieverTestProject
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.AreEqual("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        public string GetParametrizedGrenadeUrl(string action)
+        {
+            var url = $"/v1/public/single/{action}";
+
+            url += "?steamId=76561198033880857";
+
+            url += "&matchIds=1,2,3";
+
+            if (!action.Contains("overview"))
+            {
+                url += "&map=de_mirage";
+            }
+
+            return url;
         }
     }
 }
