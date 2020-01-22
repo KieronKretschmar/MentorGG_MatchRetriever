3b6e0f37b7078b765e831a630929304b8d5a0768
Kieron <kieron@mentor.gg>
2020-01-22 11:14:21 +0100

    Add classes and interfaces for LineupModels

---
+++ MatchRetriever/Models/Grenades/LineupModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.Models.Grenades
+{
+    public class LineupModel<TSample, TLineupPerformance> : SampleModel<TSample>
+        where TSample : IGrenadeSample
+        where TLineupPerformance : ILineupPerformance
+    {
+        public LineupPerformanceSummary<TLineupPerformance> UserData { get; set; }
+    }
+}
