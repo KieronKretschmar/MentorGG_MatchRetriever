7b80d6f8cbdb0c1782ac02c3bdbdc2a9d5068cf0
Kieron <kieron@mentor.gg>
2020-01-24 10:43:04 +0100

    Add MetaMatchHistory Controller, Models and Factory

---
+++ MatchRetriever/ModelFactories/MetaMatchHistoryModelFactory.cs
@@ -0,0 +1,43 @@
+﻿using MatchRetriever.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.ModelFactories
+{
+    public interface IMetaMatchHistoryModelFactory
+    {
+        Task<MetaMatchHistoryModel> GetModel(long steamId, int dailyLimit);
+    }
+
+    public class MetaMatchHistoryModelFactory : ModelFactoryBase, IMetaMatchHistoryModelFactory
+    {
+        public MetaMatchHistoryModelFactory(IServiceProvider sp) : base(sp)
+        {
+        }
+
+        public async Task<MetaMatchHistoryModel> GetModel(long steamId, int dailyLimit)
+        {
+            var allMatches = _context.PlayerMatchStats.Where(x => x.SteamId == steamId)
+                .Select(x => new MetaMatchHistoryModel.Match
+                {
+                    MatchId = x.MatchId,
+                    Map = x.MatchStats.Map,
+                    MatchDate = x.MatchStats.MatchDate,
+                })
+                .ToList();
+
+            // apply the daily limit
+            var filteredMatches = allMatches.GroupBy(x => x.MatchDate.DayOfYear)
+                .OrderByDescending(x => x.Key)
+                .SelectMany(x => x.OrderByDescending(x => x.MatchDate).Take(dailyLimit))
+                .ToList();
+
+            return new MetaMatchHistoryModel
+            {
+                Matches = filteredMatches
+            };
+        }
+    }
+}
---
+++ MatchRetriever/Models/MetaMatchHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.Models
+{
+    public class MetaMatchHistoryModel
+    {
+        public List<Match> Matches{ get; set; }
+        public struct Match
+        {
+            public long MatchId { get; set; }
+            //public Source Source { get; set; } //TODO: Source enum
+            public string Map { get; set; }
+            public DateTime MatchDate { get; set; }
+        }
+    }
+}
