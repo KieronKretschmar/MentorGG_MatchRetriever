b71c5109a15c0f655ab9b6f1e02f6fef100a908d
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-05 18:36:04 +0100

    Add friends comparison

---
+++ MatchRetriever/Controllers/v1/FriendsComparisonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MatchRetriever.Helpers;
+using MatchRetriever.ModelFactories;
+using MatchRetriever.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MatchRetriever.Controllers.v1
+{
+    [ApiVersion("1")]
+    [Route("v{version:apiVersion}/public")]
+    [ApiController]
+    public class FriendsComparisonController : BaseApiController
+    {
+        private readonly ILogger<FriendsComparisonController> _logger;
+        private readonly IFriendsComparisonModelFactory _friendsComparisonModelFactory;
+
+        public FriendsComparisonController(ILogger<FriendsComparisonController> logger, IFriendsComparisonModelFactory friendsComparisonModelFactory)
+        {
+            _logger = logger;
+            _friendsComparisonModelFactory = friendsComparisonModelFactory;
+        }
+        [Route("single/{steamId}/friends")]
+        public async Task<FriendsComparisonModel> GetFriendsComparison(long playerId, [CsvModelBinder] List<long> matchIds, int maxFriends = 3, int offset = 0)
+        {
+            var model = await _friendsComparisonModelFactory.GetModel(playerId, maxFriends, matchIds, offset);
+            return model;
+        }
+
+        [Route("single/{steamId}/friends")]
+        public Task<FriendsComparisonModel> GetFriendsComparison([CsvModelBinder] List<long> matchIds, int maxFriends = 3, int offset = 0) => GetFriendsComparison(long.Parse(User.Identity.Name), matchIds, maxFriends, offset);
+    }
+}
