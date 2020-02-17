1c4b49f11217ffd8d68a545f282b2ce2c33c2483
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-17 15:43:15 +0100

    Include detector helpers

---
+++ MatchRetriever/ModelFactories/MisplayDetectors/DetectorHelpers.cs
@@ -0,0 +1,45 @@
+﻿using Database;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace MatchRetriever.Misplays
+{
+    public class DetectorHelpers
+    {
+        private MatchContext _context;
+
+        public DetectorHelpers(IServiceProvider sp)
+        {
+            _context = sp.GetRequiredService<MatchContext>();
+        }
+
+        public List<float> TeammateDistanceToPos(long matchId, short round, int time, List<long> teammateSteamIds, Vector3 pos)
+        {
+            var teammatePositionsAtBombDrop = _context.PlayerPosition
+                .Where(x =>
+                teammateSteamIds.Contains(x.PlayerId)
+                && x.MatchId == matchId
+                && x.Round == round
+                //TODO OPTIMIZATION check if == works
+                && x.Time > time)
+                .Select(x => new
+                {
+                    x.PlayerId,
+                    x.Time,
+                    x.PlayerPos
+                })
+                .ToList()
+                .GroupBy(x => x.PlayerId)
+                .Select(x => x.OrderBy(y => y.Time).First())
+                .ToList();
+
+            return teammatePositionsAtBombDrop
+                .Select(x => Vector3.Distance(x.PlayerPos, pos))
+                .ToList();
+
+        }
+    }
+}
--- MatchRetriever/ModelFactories/MisplayDetectors/Subdetector.cs
+++ MatchRetriever/ModelFactories/MisplayDetectors/Subdetector.cs
@@ -1,4 +1,6 @@
-﻿using MatchRetriever.Models;
+﻿using Database;
+using MatchRetriever.Models;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 
@@ -7,14 +9,26 @@ namespace MatchRetriever.Misplays
     public interface ISubdetector
     {
         public Type Type { get;}
-        public ISituationCollection ComputeMisplays(long steamId, List<long> matchIds);
+        public ISituationCollection ComputeMisplays(long steamId, long matchIds);
+        public ISituationCollection FilterOutByConfig(ISituationCollection misplays);
+
     }
 
     public abstract class Subdetector<TMisplay> : ISubdetector where TMisplay : Misplay
     {
+        protected MatchContext _context;
+        protected DetectorHelpers _detectorHelpers;
+
+        public Subdetector(IServiceProvider sp)
+        {
+            _context = sp.GetRequiredService<MatchContext>();
+            _detectorHelpers = sp.GetRequiredService<DetectorHelpers>();
+        }
+
         public Type Type => typeof(TMisplay);
 
-        public abstract ISituationCollection ComputeMisplays(long steamId, List<long> matchIds);
+        public abstract ISituationCollection ComputeMisplays(long steamId, long matchIds);
 
+        public abstract ISituationCollection FilterOutByConfig(ISituationCollection misplays);
     }
 }
