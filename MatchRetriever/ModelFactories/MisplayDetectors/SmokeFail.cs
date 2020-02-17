12b8797e27c306d58974f2570877c63926695481
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-02-17 18:05:26 +0100

    Add smokefail misplay

---
+++ MatchRetriever/ModelFactories/MisplayDetectors/SmokeFail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatchRetriever.Misplays
+{
+    public class SmokeFail : Misplay
+    {
+        public int LineupId { get; internal set; }
+        public string LineupName { get; internal set; }
+    }
+}
